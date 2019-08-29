91fad133a23eb8812a3adbb35122a05937732cf2
David <david.armstrong@wustl.edu>
2019-08-29 13:47:45 -0500

    Done Studio 1!

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -6,10 +6,10 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		
-		String mood = ap.nextString("How are you feeling today?");
-		
-		System.out.println("Hello and welcome to CSE 131!");
+
+		String mood = ap.nextString ("How are you feeling today?");
+
+		System.out.println ("Hello and welcome to CSE 131!");
 		System.out.println("You seem to be feeling " + mood);
 	}
 
